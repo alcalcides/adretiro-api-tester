f975fc0977f0c34dde1045364f0e088157f2e550
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-01-04 15:19:32 -0300

    Fixes new line printing

--- app/printAllResponses.sh
+++ app/printAllResponses.sh
@@ -2,15 +2,15 @@
 
 mv ./current.txt ./older.txt
 echo "All responses" > ./current.txt
-echo $'\10' >> ./current.txt
+echo "" >> ./current.txt
 
 files=`cat ./orderOfRequests.txt`
 for eachfile in $files 
 do
 	echo /$eachfile >> ./current.txt
 	cat ./answers/$eachfile.out >> ./current.txt
-	echo $'\10' >> ./current.txt
-	echo $'\10' >> ./current.txt
+	echo "" >> ./current.txt
+	echo "" >> ./current.txt
 done
 
 echo "done"
